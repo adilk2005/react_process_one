4fd283c764378c27e62689f84d64661ef993844a
Adil khan <adilkhan@VCSs-Mac-mini-Sundas.local>
2022-04-18 10:06:06 +0500

    Adds a message

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,19 +16,24 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
+  let expenseContent = <p>No expenses found</p>;
+  if (filteredExpenses.length > 0) {
+    expenseContent = filteredExpenses.map(expense => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+      ));
+  }
+
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
 
       {/* {props.items.map(expense => (  */}
-      {filteredExpenses.map(expense => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-        ))}
+      {expenseContent}
     </Card>
   );
 }
